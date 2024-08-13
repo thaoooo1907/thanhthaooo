6b4ca00a17c535ce19a298bb6e3e7953088116af
Thanhthao621 <thanhthao19077@gmail.com>
2024-08-13 21:27:47 +0700

    Add files via upload

---
+++ diem_trung_binh.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 18:37:54 2024
+
+@author: Student
+NGUYEN THI THANH THAO - 23705621 
+bai 4:
+    GPA = float (input("nhap diem trung binh (GPA):"))
+if GPA < 3.5:
+    print ("hoc luc kem")
+elif 3.5 <= GPA < 5.0 :
+    print ("hoc luc yeu")
+elif 5.0 <= GPA < 7.0 :
+    print ("hoc luc trung binh")
+elif 7.0 <= GPA < 8.0 :
+    print ("hoc luc kha")
+elif 8.0 <= GPA < 9.0 :
+    print ("hoc luc gioi")
+elif 9.0 <= GPA < 10 :
+    print ("hoc luc xuat sac")
+    
+    ---------------------
+    bai 5
+    distance = float(input("nhap do dai doan duong den truong:"))
+if distance > 1200:
+    print ("duong den truong qua xa")
+else:
+    print ("kgong xac dinh duoc xa-gan")
+    ---------------------------------------
+    bai 1
+    a = float (input("nhap a"))
+b = float (input("nhap b:"))
+if a == 0 and b != 0:
+    print ("phuong trinh co vo so nghiem")
+elif a== 0 and b == 0 :
+    print ("phuong trinh co vo so nghiem")
+else: 
+    print ("nghiem cua phuong trinh la: " , -b/a)
+    
+
+"""
+
+GPA = float (input("nhap diem trung binh (GPA):"))
+if GPA < 3.5:
+    print ("hoc luc kem")
+elif 3.5 <= GPA < 5.0 :
+    print ("hoc luc yeu")
+elif 5.0 <= GPA < 7.0 :
+    print ("hoc luc trung binh")
+elif 7.0 <= GPA < 8.0 :
+    print ("hoc luc kha")
+elif 8.0 <= GPA < 9.0 :
+    print ("hoc luc gioi")
+elif 9.0 <= GPA < 10 :
+    print ("hoc luc xuat sac")
+    
